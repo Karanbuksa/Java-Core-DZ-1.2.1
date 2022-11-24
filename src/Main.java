34bbac4bf3c44f25fd83f42bd509e49f4ddde85d
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2022-11-24 16:23:25 +0500

    Изучение стримов

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> intListModified = new LinkedList<>();
+
+
+        for (Integer integer : intList) {
+            if (integer > 0 && integer % 2 == 0) {
+                intListModified.add(integer);
+            }
+        }
+
+        IntegerComparator integerComparator = new IntegerComparator();
+        intListModified.sort(integerComparator);
+        System.out.println(intListModified);
+    }
+}
+
+class IntegerComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return Integer.compare(o1, o2);
+    }
+}
